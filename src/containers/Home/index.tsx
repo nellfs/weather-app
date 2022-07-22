ba805eaf7780ba3a13a205078b7fcf0bc4553db7
nellfs <pokycelit@gmail.com>
2022-07-22 18:54:56 -0300

    api

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,7 +1,23 @@
 import "./index.css";
+import "./clouds.css";
 
 const Home = () => {
-  return <div className="home">Weather</div>;
+  return (
+    <div className="background">
+      <div className="cloud one" />
+      <div className="cloud two" />
+      <div className="cloud three" />
+
+      <div className="home">Weather Locator</div>
+      <form className="form">
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
